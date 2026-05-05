d21dd583603d8e5583889264894ccc1b516cba68
NassCa <nassca@protonmail.com>
2026-05-05 09:06:45 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE='icd-v1';
+const FILES=['./ICDBrowser.html','./manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(
+  caches.open(CACHE).then(c=>c.addAll(FILES))
+));
+self.addEventListener('fetch',e=>e.respondWith(
+  caches.match(e.request).then(r=>r||fetch(e.request))
+));
